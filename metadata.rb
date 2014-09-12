75a8fc2a6c3afbbbbdb1b7c5f4eb336dda2ba909
Kyle Boorky <kboorky@turbine.com>
2014-09-12 10:04:18 -0400

    updating metadata

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,7 @@
-name              'go-sandbox'
-maintainer        'Colin Hubert'
-maintainer_email  'chubert@turbine.com'
+name              'go-celery-api'
+maintainer        'Kyle Boorky'
 license           'Apache 2.0'
-description       'Vagrant instance of golang'
+description       'Golang API for celery task creation'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '1.0.0'
 
